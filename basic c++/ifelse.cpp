d6bbd283aceba21f967d1ceba29f5b48d22ee69b
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 20:17:00 +0530

    if else proble 1

---
+++ basic c++/ifelse.cpp
@@ -0,0 +1,14 @@
+//if else problem
+//print if you are above 18 then you are adult or you are noy adult
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int age;
+    cin>>age;
+    if (age>=18){
+        cout<<"you are an adult";
+    }
+    else {
+        cout<<"you are not an adult";
+    }
+}
