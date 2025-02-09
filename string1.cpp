1b885792a8620e4f95a5f916bd7656923bbf4053
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 14:52:00 +0530

    adding a string in one line

---
+++ string1.cpp
@@ -0,0 +1,13 @@
+//adding a string in one line
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1;
+    string s2;
+    string str;
+    getline(cin,str);
+    //getline use for take all strings of single line
+    cin>>s1>>s2;
+    cout<<s1<<" "<<s2;
+    return 0;
+}
