5e2e30d4bfc825315e59d48d762e18732f418e1c
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 14:37:16 +0530

    string operation in c+=

---
+++ string.cpp
@@ -0,0 +1,12 @@
+//string and character in c++
+//string is used for define line or quote
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1;
+    string s2;
+    string s3;
+    cin>>s1>>s2>>s3;
+    cout<<s1<<" "<<s2<<" "<<s3;
+    return 0;
+}
