cd736c21700d55e2650a3fe69aac8a0b68c977a9
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 14:04:42 +0530

    //long double- long double precision floating point data type //long long- long long integer data type

---
+++ basic c++/intlonglongdoublefloat.cpp
@@ -0,0 +1,23 @@
+//long double- long double precision floating point data type
+//long long- long long integer data type
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x;
+    long long y;
+    //long long is used to declare a variable of long long integer type
+    cout<<"Enter two numbers:";
+    cin>>x>>y;
+    cout<<x+y;
+}
+//double- double precision floating point data type
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double x;
+    double y;
+    //double is used to declare a variable of double precision floating point type
+    cout<<"Enter two numbers:";
+    cin>>x>>y;
+    cout<<x+y;
+}
