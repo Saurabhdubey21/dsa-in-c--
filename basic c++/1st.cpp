f661070e4731c95752e8662dd561e550849db1d1
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 10:50:55 +0530

    understand the basic of c++, use of cout.

---
+++ basic c++/1st.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+//we use <bits/stdc++.h> to include all the libraries of c++
+//if we use #include<iostream> then we have to include all the libraries separately
+//like #include<cmath> for math functions
+using namespace std;
+//using namespace std is used to avoid writing std:: before every function of c++
+int main()
+//main function is the starting point of the program
+{
+    cout<<"Hello World";
+    //cout is used to print the output on the screen
+    return 0;
+    //return 0 is used to end the program and 0 is returned to the operating system
+}
