079f39117a248f48d36113ddd1f770f618f611ef
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 10:56:55 +0530

    integer type of data

---
+++ basic c++/2nd.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    //int is used to declare a variable of integer type
+    cin>>x>>y;
+    //cin is used to take input from the user
+    //>> is used to take input from the user    
+    cout<<x+y;
+}
