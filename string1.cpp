e913d8f01f893f63f272fb5e1631db6afdf55993
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 15:37:07 +0530

    print all strings of single line

--- string1.cpp
+++ string1.cpp
@@ -2,12 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s1;
-    string s2;
     string str;
     getline(cin,str);
     //getline use for take all strings of single line
-    cin>>s1>>s2;
-    cout<<s1<<" "<<s2;
+    cout<<str;
     return 0;
 }
