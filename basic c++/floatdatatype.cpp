f48bb1d37b698ede24cac1ab2bdc44d07d6c08e6
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 13:47:02 +0530

    float data type in c++

---
+++ basic c++/floatdatatype.cpp
@@ -0,0 +1,11 @@
+//float type data type
+//float is used to store floating(decimal)point numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float x;
+    float y;
+    cout<<"Enter two numbers:";
+    cin>>x>>y;
+    cout<<x+y;
+}
