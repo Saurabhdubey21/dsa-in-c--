299be08dcd1164cc44761798467c54b698c3bdef
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 13:23:17 +0530

    data types in c++

---
+++ basic c++/datatypes.cpp
@@ -0,0 +1,27 @@
+//datatypes- data types in c++
+//data types are used to define the type of data a variable can store
+//int- integer data type    
+//float- floating point data type
+//char- character data type
+//double- double precision floating point data type
+//long- long integer data type
+//short- short integer data type
+//string- string data type
+//long long- long long integer data type
+//unsigned- unsigned integer data type
+//signed- signed integer data type
+//void- void data type
+//long double- long double precision floating point data type
+//size_t- size data type
+//ptrdiff_t- pointer difference data type
+//this program takes two integers as input and prints their sum
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cin>>x>>y;
+    // >> is used to take input from the user
+    // cin is used to take input from the user
+    cout<<x+y;
+}
