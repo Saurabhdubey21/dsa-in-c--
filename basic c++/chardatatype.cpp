0c83c262ba280d7cdfebadb0916834b951bc203c
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-09 13:57:16 +0530

    char datatype in c++

---
+++ basic c++/chardatatype.cpp
@@ -0,0 +1,11 @@
+//character data type
+//char is used to store a single character
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char x;
+    //char is used to declare a variable of character type
+    cout<<"Enter a character:";
+    cin>>x;
+    cout<<x;
+}
