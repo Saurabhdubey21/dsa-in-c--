50f5cb959427c2f3b0f1c373e1b1965442459c02
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-10 14:03:45 +0530

    nested loop

--- basic c++/nestedifelse.cpp
+++ basic c++/nestedifelse.cpp
@@ -23,3 +23,23 @@ int main(){
         cout<<"retirement time";
     }
 }
+//nested loop
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cout<<"age:";
+    cin>>age;
+    if(age<18){
+        cout<<"Not eligible for jobs";
+    }
+    else if(age<=57){
+        cout<<"eligible for jobs";
+        if(age>=55){
+            cout<<" but retirement soon";
+        }
+    }
+    else{
+        cout<<"retirement time";
+    }
+}
