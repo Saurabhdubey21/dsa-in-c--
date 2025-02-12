8e3ddac889143b3b8b41fad3662b4bd611e19b9c
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-12 19:48:22 +0530

    loop problem 1

--- basic c++/loops.cpp
+++ basic c++/loops.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main(){
     int i;
-    for(int i=0;i<=10;i=i+1){
+    //here we take i for get the value of i
+    for(i=0;i<=10;i=i+1){
+// Starts with i at 0,
+// Continues to execute as long as i is 10 or less than 10,
+// increase i by 1 after printing one output
         cout<<"Saurabh"<<endl;
     }
     cout<<i<<endl;
