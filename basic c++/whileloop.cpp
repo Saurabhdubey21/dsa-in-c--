4a77abede4ebf43e1cdc9579a47f36071557ad43
Saurabhdubey21 <mkd221524@gmail.com>
2025-02-13 20:47:10 +0530

    while loop in c++

---
+++ basic c++/whileloop.cpp
@@ -0,0 +1,18 @@
+// A while loop in C++ is a control flow statement that repeatedly executes a block of code as long as a specified condition is true.
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int i=2;
+    // while(i<=5){
+    //     cout<<"saurabh"<<i<<endl ;
+    //     i=i+1;
+    // }
+    // while(i<=1);
+    do{
+        cout<<"Saurabh"<<i<<endl;
+        i=i+1;
+    }
+    while(i<=1);
+    cout<<i<<endl;
+    return 0;
+}
