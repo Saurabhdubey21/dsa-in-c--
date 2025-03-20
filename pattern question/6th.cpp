6ffe34e1eb66aad8cc9fa3f86fa834e6e83a4780
Saurabhdubey21 <mkd221524@gmail.com>
2025-03-21 01:05:14 +0530

    print decreasing no of pattern

--- pattern question/6th.cpp
+++ pattern question/6th.cpp
@@ -1,23 +1,17 @@
-//print*****
-//     ****
-//     ***
-//     **
-//     *
 #include<bits/stdc++.h>
 using namespace std;
 void print1(int n){
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++)
-        {
-            cout<<"*"; 
+        for(int j=2;j<=i+1;j++){
+            cout<<i<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
 }
 int main(){
     int t;
     cin>>t;
-    for(int i=0;i<t;i++){
+    for(int i=0; i<t;i++){
         int n;
         cin>>n;
         print1(n);
