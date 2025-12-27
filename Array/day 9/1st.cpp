513cccd060dad74530fd2cc1c9ae2498a4ffaf3f
Saurabhdubey21 <mkd221524@gmail.com>
2025-12-27 22:20:35 +0530

    best time to buy and sell stock

---
+++ Array/day 9/1st.cpp
@@ -0,0 +1,20 @@
+//best time to sell and buy stock
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of days: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter the stock price per days: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int mini= arr[0];
+    int maxProfit =0;
+    for(int i=0; i<n;i++){
+        int cost =arr[i]-mini;
+        maxProfit=max(mini,arr[i]);
+    }
+    cout<<"Maximum return of day: "<<maxProfit;
+}
