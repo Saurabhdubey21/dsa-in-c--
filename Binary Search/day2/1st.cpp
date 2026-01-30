80c25216bb2dee20943cdc36b39b4ec4b076aca1
Saurabhdubey21 <mkd221524@gmail.com>
2026-01-30 21:51:50 +0530

    upper bound and lower bound

---
+++ Binary Search/day2/1st.cpp
@@ -0,0 +1,38 @@
+//lower bound implementation
+//Given a sorted array arr[] and a number target,
+// the task is to find the lower bound of the target in this given array. 
+#include<bits/stdc++.h>
+using namespace std;
+int lowerbound(vector<int>&arr,int n,int target){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter the size of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter the target: ";
+    cin>>target;
+    int result=lowerbound(arr,n,target);
+    if(result==n)
+    cout<<"Result does not found at: "<<endl;
+    else cout<<"target found at index: "<<result;
+}
